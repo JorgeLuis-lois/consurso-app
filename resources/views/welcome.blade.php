e5fcf7c9326c0f1b3de9ea1ed68e042c285506af
Ismael Gomez <igomezp.pro@gmail.com>
2022-04-30 12:18:19 -0500

    IGOMEZ -  Diseño

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,9 @@
     <link href="{{ asset('img/favicon.ico') }}" rel="icon">
 
     <!-- Google Web Fonts -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Rancho&effect=shadow-multiple|3d-float">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
@@ -31,6 +32,12 @@
 
     <!-- Template Stylesheet -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Pangolin', serif;
+            font-size: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -48,29 +55,31 @@
         <div class="container-xxl position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                 <a href="" class="navbar-brand p-0">
-                    <h1 class="m-0"><i class="fa fa-search me-2"></i>Feria<span class=""> de Conocimientos</span></h1>
+                    <h1 class="m-0"><i class="fa fa-search me-2"></i>Feria<span class="fs-3"> de Conocimientos</span></h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="fa fa-bars"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
-                    @if (Route::has('login'))
+                        @if (Route::has('login'))
                         @auth
                         <a href="index.html" class="nav-item nav-link active">Home</a>
                         @else
-                        <a href="{{ route('login') }}" class="nav-item nav-link active">Log in</a>                        
                         <a href="#mu-participar-overview" class="nav-item nav-link">Como participar</a>
                         <a href="#mu-pueden-participar-overview" class="nav-item nav-link">Quiénes pueden participar</a>
-                        <a href="#mu-informacion-overview" class="nav-item nav-link">Temas</a>
-                        <a href="#mu-premios-overview" class="nav-item nav-link">Premios</a>                        
+                        <a href="#mu-premios-overview" class="nav-item nav-link">Premios</a>
                         @endauth
-                    @endif    
+                        @endif
                     </div>
                 </div>
             </nav>
         </div>
         <!-- Navbar & Hero End -->
+
+
+
+
 
         <!-- Hero -->
         <div class="container-xxl position-relative p-0">
@@ -80,7 +89,7 @@
                         <div class="col-lg-6 text-center text-lg-start">
                             <h1 class="text-white mb-4 animated zoomIn">Cuidando mis sueños</h1>
                             <p class="text-white pb-3 animated zoomIn">El proyecto “Yo me cuido”, presenta el concurso “Cuidando mis sueños”, que tiene por finalidad compartir información que contribuya al desarrollo integral de las/los adolescentes, teniendo en cuenta el enfoque de derechos, género, diversidad, interculturalidad e inclusión.</p>
-                            <a href="{{ route('login') }}" class="btn btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Paticipar</a>
+                            <a href="{{ route('login') }}" class="btn btn-outline-info btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft btn-lg">Participar</a>
                         </div>
                         <div class="col-lg-6 text-center text-lg-start">
                             <img class="img-fluid" src="{{ asset('img/portada.png') }}" alt="">
@@ -90,34 +99,19 @@
             </div>
         </div>
         <!-- Hero End -->
-        <!-- Full Screen Search Start -->
-        <div class="modal fade" id="searchModal" tabindex="-1">
-            <div class="modal-dialog modal-fullscreen">
-                <div class="modal-content" style="background: rgba(29, 29, 39, 0.7);">
-                    <div class="modal-header border-0">
-                        <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body d-flex align-items-center justify-content-center">
-                        <div class="input-group" style="max-width: 600px;">
-                            <input type="text" class="form-control bg-transparent border-light p-3" placeholder="Type search keyword">
-                            <button class="btn btn-light px-4"><i class="bi bi-search"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Full Screen Search End -->
-
 
         <!-- About Start -->
         <div class="container-xxl py-5" id='mu-participar-overview'>
+
+
+
             <div class="container px-lg-5">
                 <div class="row g-5">
                     <div class="col-lg-8 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="section-title position-relative mb-4 pb-2">
                             <h2 class="mt-2">¿Cómo participar?</h2>
                         </div>
-                        
+
                         <div class="row g-3">
                             <div class="col-sm-12">
                                 <h6 class="mb-3"><i class="fa fa-check text-primary me-2"></i>Fortalece tus conocimientos, leyendo la información adjunta</h6>
@@ -127,59 +121,76 @@
                             </div>
                         </div>
                         <div class="d-flex align-items-center mt-4">
-                            <a class="btn btn-primary rounded-pill px-4 me-3" href="">Ir a Documentos</a>
+                            <!--   <a class="btn btn-primary rounded-pill px-4 me-3" href="">Ir a Documentos</a>-->
                         </div>
                     </div>
                     <div class="col-lg-4">
-                        <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('img/about.jpg') }}">
+                        <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('img/part.png') }}">
                     </div>
                 </div>
             </div>
+
+
+
+
         </div>
         <!-- About End -->
 
         <!-- About Start -->
-        <div class="container-xxl py-5" id ="mu-pueden-participar-overview">
+
+        <div class="container-xxl py-5" id='mu-participar-overview'>
+
+
             <div class="container px-lg-5">
                 <div class="row g-5">
                     <div class="col-lg-8 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="section-title position-relative mb-4 pb-2">
-                            <h2 class="mt-2">¿QUIÉNES PUEDEN PARTICIPAR?</h2>
+                            <h2 class="mt-2">¿Quienes pueden partcipar?</h2>
                         </div>
-                        <p class="mb-4">Asociación Pataz, en el marco de su proyecto “Yo me Cuido”, presenta el Concurso “Cuidando mis sueños” dirigido a adolescentes (12 años a 17 años 11 meses 29 días) que estudian en los anexos de Chagual, Vista Florida, Suyubamba, Pataz, Zarumilla, Vijus, Nimpana y Chuquitambo. El sorteo se llevará a cabo el 20 de julio del 2022 y sólo será válido para las/los adolescentes que estudian en las zonas donde intervine el Proyecto y que hayan seguido la Ruta para participar.</p>
+                        <p class="mb-3">Asociación Pataz, en el marco de su proyecto “Yo me Cuido”, presenta el Concurso “Cuidando mis sueños” dirigido a adolescentes (12 años a 17 años 11 meses 29 días) que estudian en los anexos de Chagual, Vista Florida, Suyubamba, Pataz, Zarumilla, Vijus, Nimpana y Chuquitambo. El sorteo se llevará a cabo el 20 de julio del 2022 y sólo será válido para las/los adolescentes que estudian en las zonas donde intervine el Proyecto y que hayan seguido la Ruta para participar.</p>
                         <div class="row g-3">
                         </div>
                         <div class="d-flex align-items-center mt-4">
-                            <a class="btn btn-primary rounded-pill px-4 me-3" href="">Ir a Documentos</a>
+                            <!--   <a class="btn btn-primary rounded-pill px-4 me-3" href="">Ir a Documentos</a>-->
                         </div>
                     </div>
                     <div class="col-lg-4">
-                        <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('img/about.jpg') }}">
+                        <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('img/comop.png') }}">
                     </div>
                 </div>
             </div>
+
+
         </div>
+
         <!-- About End -->
         <!-- Service Start -->
+
+
         <div class="container-xxl py-5" id="mu-informacion-overview">
             <div class="container px-lg-5">
+
+
                 <div class="section-title position-relative text-center mb-5 pb-2 wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 class="position-relative d-inline text-primary ps-4">Información</h6>
                     <h2 class="mt-2">Fortalece tus conocimientos, leyendo la información adjunta</h2>
                 </div>
                 <div class="row g-4">
-                @foreach ($temas as $tema)
-                    <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.1s">
-                        <div class="service-item d-flex flex-column justify-content-center text-center rounded">
-                            <div class="service-icon flex-shrink-0">
-                                <i class="fa fa-home fa-2x"></i>
+                    <div class="accordion" id="accordionExample">
+                    @foreach ($temas as $tema)
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                {{$tema->valoradicional}}
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <strong>{{$tema->nombres}}</strong>
+                                </div>
                             </div>
-                            <h5 class="mb-3">{{$tema->valoradicional}}</h5>
-                            <p>{{$tema->nombres}}</p>
-                            <a class="btn px-3 mt-auto mx-auto" href="">Descargar información</a>
                         </div>
+                        @endforeach
                     </div>
-                @endforeach
                 </div>
             </div>
         </div>
@@ -190,7 +201,6 @@
         <div class="container-xxl py-5" id="mu-premios-overview">
             <div class="container px-lg-5">
                 <div class="section-title position-relative text-center mb-5 pb-2 wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 class="position-relative d-inline text-primary ps-4">Sorteo</h6>
                     <h2 class="mt-2">Premios</h2>
                 </div>
                 <div class="row mt-n2 wow fadeInUp" data-wow-delay="0.1s">
@@ -250,12 +260,12 @@
                 <div class="copyright">
                     <div class="row">
                         <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                            &copy; <a class="border-bottom" href="#">FERIA DE CONOCIMIENTOS</a>, Cuidando mis sueños.							
-						</div>
+                            &copy; <a class="border-bottom" href="#">FERIA DE CONOCIMIENTOS</a>, Cuidando mis sueños.
+                        </div>
                         <div class="col-md-6 text-center text-md-end">
                             <div class="footer-menu">
-                                <a href="">Home</a>
-                                <a href="">Help</a>
+                                <a href=""></a>
+                                <a href=""></a>
                             </div>
                         </div>
                     </div>
@@ -268,7 +278,6 @@
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top pt-2"><i class="bi bi-arrow-up"></i></a>
     </div>
-
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
